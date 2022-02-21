f96adce0822e16495713c946af0cf5c0e09a9ff0
RyanShaw3 <ryanjshaw@gmx.com>
2022-02-21 08:55:58 +0800

    add pipe.h

---
+++ JShell/pipe.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "data.h"
+#include "redirect.h"
+
+//调用管道命令
+int callPipeCommand(int, int);
