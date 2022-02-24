ad17e1d272c8983f4129e323c261e76464c2b167
RyanShaw3 <ryanjshaw@gmx.com>
2022-02-24 08:51:02 +0800

    add prompt.h

---
+++ JShell/prompt.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <sys/types.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "data.h"
+
+//获取用户名
+void getUserName();
+//获取主机名
+void getHostName();
+//获取提示符
+void getPromptSign();
+//获取提示路径
+void getPromptPath();
+//打印提示
+void printPrompt();
