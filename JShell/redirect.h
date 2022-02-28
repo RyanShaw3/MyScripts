778f5c47dd3a9fe88d07abca43ccfad7299400ca
RyanShaw3 <ryanjshaw@gmx.com>
2022-02-28 08:57:46 +0800

    add redirect.h

---
+++ JShell/redirect.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "data.h"
+
+//调用重定向命令
+int callRedirectCommand(int, int);
