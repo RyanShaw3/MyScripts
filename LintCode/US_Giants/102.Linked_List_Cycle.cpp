7757e895c4c3ee34fe4528af5feb0f40e41b03b0
RyanShaw3 <ryanjshaw@gmx.com>
2022-06-13 08:47:47 +0800

    add 102.Linked_List_Cycle.cpp

---
+++ LintCode/US_Giants/102.Linked_List_Cycle.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition of singly-linked-list:
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *        this->val = val;
+ *        this->next = NULL;
+ *     }
+ * }
+ */
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    bool hasCycle(ListNode *head)
+    {
+        // write your code here
+        if (head == NULL)
+            return false;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
