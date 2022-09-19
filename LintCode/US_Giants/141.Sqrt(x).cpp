216b2b238b136a22c8306e8301b11d1679d0f73e
RyanShaw3 <ryanjshaw@gmx.com>
2022-09-19 09:30:53 +0800

    141.Sqrt(x).cpp

---
+++ LintCode/US_Giants/141.Sqrt(x).cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int x)
+    {
+        // write your code here
+        return pow(x, 0.5);
+    }
+};
