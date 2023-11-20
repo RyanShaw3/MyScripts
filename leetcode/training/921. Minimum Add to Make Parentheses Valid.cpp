a0ac31f3b221d7d0d72b843af99b8cf9b97cfaa3
RyanShaw3 <ryanjshaw@gmx.com>
2023-11-20 09:38:29 +0800

    add 921. Minimum Add to Make Parentheses Valid.cpp

---
+++ leetcode/training/921. Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int minAddToMakeValid(string s) {
+    int ans = 0;
+    int leftCount = 0;
+    for (auto &c : s) {
+      if (c == '(') {
+        leftCount++;
+      } else {
+        if (leftCount > 0) {
+          leftCount--;
+        } else {
+          ans++;
+        }
+      }
+    }
+    ans += leftCount;
+    return ans;
+  }
+};
