5eb410c859907a4d74f18a593f679130112b34d5
RyanShaw3 <ryanjshaw@gmx.com>
2024-02-05 09:22:05 +0800

    add 796. Rotate String.cpp

---
+++ leetcode/training/796. Rotate String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    bool rotateString(string s, string goal)
+    {
+        return s.size() == goal.size() && (s + s).find(goal) != string::npos;
+    }
+};
