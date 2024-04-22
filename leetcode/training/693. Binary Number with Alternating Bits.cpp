8bfb32bee965ba68b38044c92fdbd4ec37255504
RyanShaw3 <ryanjshaw@gmx.com>
2024-04-22 09:16:17 +0800

    add 693. Binary Number with Alternating Bits.cpp

---
+++ leetcode/training/693. Binary Number with Alternating Bits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool hasAlternatingBits(int n)
+    {
+        long a = n ^ (n >> 1);
+        return (a & (a + 1)) == 0;
+    }
+};
