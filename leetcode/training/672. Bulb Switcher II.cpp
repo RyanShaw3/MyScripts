6963cf3cd5115c2e4142aa62d599b720940a2e6c
RyanShaw3 <ryanjshaw@gmx.com>
2024-05-06 09:21:51 +0800

    add 672. Bulb Switcher II.cpp

---
+++ leetcode/training/672. Bulb Switcher II.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int flipLights(int n, int presses)
+    {
+        if (presses == 0)
+            return 1;
+        if (n == 1)
+            return 2;
+        if (n == 2)
+            return presses == 1 ? 3 : 4;
+        if (presses == 1)
+            return 4;
+        if (presses == 2)
+            return 7;
+        return 8;
+    }
+};
