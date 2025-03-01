ef5417ca896eed1876d103656cb909f4bf2428b2
RyanShaw3 <ryanjshaw@gmx.com>
2025-03-01 09:14:41 +0800

    add 面试题 17.19. Missing Two LCCI.cpp

---
+++ leetcode/training/面试题 17.19. Missing Two LCCI.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> missingTwo(vector<int>& nums) {
+    long n = nums.size() + 2;
+    int a = -accumulate(nums.cbegin(), nums.cend(), -(1 + n) * n / 2);
+    int b = -inner_product(nums.cbegin(), nums.cend(), nums.cbegin(),
+                           -(1 + n) * n / 2 * (2 * n + 1) / 3);
+    int tmp = sqrt(2 * b - a * a);
+    return {(a + tmp) / 2, (a - tmp) / 2};
+  }
+};
+
+// 求b的过程完全没想到
